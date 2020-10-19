74b18549c9ef322250e5ebfa2152ea378460c304
dhruv <dhruv.sengupta04@gmail.com>
2020-10-19 08:52:32 -0700

    added basic modules to mainmenu

---
+++ src/view_control/mainmenu.java
@@ -0,0 +1,13 @@
+package view_control;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.EventQueue;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
