86836020f95fe8ebb6b88272f41bc1de5dc3cb68
Tanay Shah <tanay.v.shah@gmail.com>
2020-10-20 09:27:24 -0700

    model constructor outline

--- src/model/CalculatorModel.java
+++ src/model/CalculatorModel.java
@@ -1,4 +1,18 @@
 package model;
+import control.MathCalc.OPERATOR;
 
 public class CalculatorModel {
+    //calcAnswer = control.MathCalc.calculateIt(arg1, mathOp, arg2);
+    //calcArea.setText(String.valueOf(calcAnswer));
+    //arg1 = Double.parseDouble(calcArea.getText());
+    //mathState = STATE.CALC;
+    //initialCalcAreaInputState = true;
+
+
+    public CalculatorModel() {
+    }
+
+    public CalculatorModel(int arg1, OPERATOR mathOp, int arg2) {
+
+    }
 }
