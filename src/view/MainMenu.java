ee5ee5ab550da5b9d3b4ca2efdd1141dd9887577
DhruvK0 <46884021+DhruvK0@users.noreply.github.com>
2020-10-21 09:03:40 -0700

    Added Main Menu file

--- src/view/MainMenu.java
+++ src/view/MainMenu.java
@@ -1,2 +1,108 @@
 package view;
 
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.EventQueue;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+public class MainMenu extends JFrame {
+    /**
+     Launch the MainMenu to display sub features
+     */
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                MainMenu frame = new MainMenu();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public MainMenu() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 450, 300);
+
+//        // JLabel with image
+//        JLabel pic = new JLabel("");
+//        java.awt.Image image = new ImageIcon("APCompSci.jpeg").getImage();
+//        pic.setIcon(new ImageIcon(image));
+//        pic.setBounds(377, 111, 128, 128);
+
+        // Content Panel to add Label and Image
+        //https://docs.oracle.com/javase/tutorial/uiswing/components/rootpane.html
+        Container content = getContentPane();
+        content.setBackground(new Color(100,100,255));
+//        content.add(pic);
+
+        // Menu Objects
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        JMenu mnSuite = new JMenu("Suite");
+        menuBar.add(mnSuite);
+
+//        JMenuItem mntmPreferences = new JMenuItem("Temperature Convertor");
+//        mnHone.add(mntmPreferences);
+//
+//        JMenu mnApTest = new JMenu("Physics Solver");
+//        menuBar.add(mnApTest);
+//
+//        JMenuItem mntmApReview = new JMenuItem("Review");
+//        mntmApReview.addActionListener(e -> {
+//            AP_UI frame = new AP_UI();
+//            frame.setVisible(true);
+//        });
+//        mnApTest.add(mntmApReview);
+//
+//        JMenu mnJig = new JMenu("Labs");
+//        menuBar.add(mnJig);
+
+        JMenuItem mntmCalculator = new JMenuItem("Scientific Calculator");
+        mntmCalculator.addActionListener(e -> {
+            CalculatorUI frame = new CalculatorUI();
+            frame.setVisible(true);
+        });
+        mnSuite.add(mntmCalculator);
+
+        JMenuItem mntmPhysicsSolve = new JMenuItem("Physics Solver");
+        mntmPhysicsSolve.addActionListener(e -> {
+            MethodSearchUI frame = new MethodSearchUI();
+            frame.setVisible(true);
+        });
+
+        JMenuItem mntmTempConv = new JMenuItem("Temperature Convertor");
+        mntmTempConv.addActionListener(e -> {
+            PalindromeUI frame = new PalindromeUI();
+            frame.setVisible(true);
+        });
+        mnSuite.add(mntmTempConv);
+        mnSuite.add(mntmPhysicsSolve);
+
+//        JMenuItem mntmProgrammingLanguages = new JMenuItem("Model Search");
+//        mntmProgrammingLanguages.addActionListener(e -> {
+//            ProgLang_UI frame = new ProgLang_UI();
+//            frame.setVisible(true);
+//        });
+//        mnJig.add(mntmProgrammingLanguages);
+
+
+    }
+
+}
+}
