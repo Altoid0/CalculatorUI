c34cea87f04af6407172044305ec8a1eefd48913
DhruvK0 <46884021+DhruvK0@users.noreply.github.com>
2020-10-23 08:24:58 -0700

    Added comments to MathCalc

--- src/control/MathCalc.java
+++ src/control/MathCalc.java
@@ -1,7 +1,7 @@
 package control;
 import java.lang.Math;
 public class MathCalc {
-	
+	// Updated enumerator list to include new functions
 	public static enum OPERATOR { NOOP, PLUS, MINUS, DIVIDE, MULTIPLY, MODULUS, EXPONENT, LOG };
 	public static double calculateIt(double arg1, OPERATOR mathOp, double arg2) {
 
@@ -14,18 +14,23 @@ public class MathCalc {
 	        case MINUS:
 	            calcAnswer = arg1 - arg2;
 	            break;
+	            // Added Divide Button: Dhruv K
 	        case DIVIDE:
 	            calcAnswer = arg1 / arg2;
 	            break;
+	            // Added Multiply Button: Dhruv K
 	        case MULTIPLY:
 	            calcAnswer = arg1 * arg2;
 	            break;
+	            // Added Modulus Button: Dhruv K
 			case MODULUS:
 				calcAnswer = arg1 % arg2;
 				break;
+				// Added Exponent Button: Dhruv K
 			case EXPONENT:
 				calcAnswer = Math.pow(arg1,arg2);
 				break;
+				// Added Log Button: Gautam G
 			case LOG:
 				calcAnswer = Math.log10(arg1);
 				break;
